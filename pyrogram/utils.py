a609a85a1cba7312b02dec209481592d7482476f
Vasyl Cherpak <120051559+Vasab1tch@users.noreply.github.com>
2024-07-24 12:44:12 +0300

    Update utils.py

--- pyrogram/utils.py
+++ pyrogram/utils.py
@@ -234,16 +234,13 @@ def get_peer_id(peer: raw.base.Peer) -> int:
 
 
 def get_peer_type(peer_id: int) -> str:
-    if peer_id < 0:
-        if MIN_CHAT_ID <= peer_id:
-            return "chat"
-
-        if MIN_CHANNEL_ID <= peer_id < MAX_CHANNEL_ID:
-            return "channel"
-    elif 0 < peer_id <= MAX_USER_ID:
+    peer_id_str = str(peer_id)
+    if not peer_id_str.startswith("-"):
         return "user"
-
-    raise ValueError(f"Peer id invalid: {peer_id}")
+    elif peer_id_str.startswith("-100"):
+        return "channel"
+    else:
+        return "chat"
 
 
 def get_channel_id(peer_id: int) -> int:
